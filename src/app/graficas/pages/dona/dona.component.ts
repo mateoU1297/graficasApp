5f39ff56f90a49fffaddf6dc1251bed7ef8ec672
mateoU1297 <castro.mateo@correounivalle.edu.co>
2022-07-18 23:09:33 -0500

    se crea gráfica de dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
   styleUrls: ['./dona.component.css']
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [ 350, 450, 100, 150 ]
+  ];
 
-  ngOnInit(): void {
-  }
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    { 
+      backgroundColor: [
+        '#59B8FF',
+        '#E04F3F',
+        '#B8F752',
+        '#5A6FE0',
+      ]
+    }
+  ];
 
 }
